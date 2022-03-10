a25cb76bb4064cfdd38094e721226c9fe43a31aa
kbventures <kmb_x@hotmail.com>
2022-03-10 12:27:56 -0500

    Install Webpack Webpack  Server Html Webpack Plugin Injecting JS

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// Entry Point
+
+console.log('My First Web App');
+document.querySelector('#fromjs').innerHTML = `<h1>From index.js</h1>`;
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    mode: 'development',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template:
+          './public/index.html',
+      }),
+    ],
+  };
