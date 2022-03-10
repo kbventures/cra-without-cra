4e747c7cc6a51a8b9b18c6a704b65f5b11bfcf5e
kbventures <kmb_x@hotmail.com>
2022-03-10 13:43:18 -0500

    Added Babel To Webpack

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-console.log('My First Web App');
+
+// console.log('My First Web App');
+
+// Will Work Without Babel
+// ReactDOM.render(
+//     'Hello from index.js',
+//     document.querySelector(
+//       '#fromjs'
+//     )
+//   );
+
+
+// Will Only Work With Babel Module
 ReactDOM.render(
-    'Hello from index.js',
-    document.querySelector(
-      '#fromjs'
-    )
-  );
+  <h1>Hello from js</h1>,
+  document.querySelector(
+    '#fromjs'
+  )
+);
